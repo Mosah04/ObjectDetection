9d4528ad821aba62ae1eb036b85287477885f400
unknown <moussahaad277@gmail.com>
2024-09-20 10:56:24 +0100

    FixedBatchNormalization.py fixing 1

--- model/FixedBatchNormalization.py
+++ model/FixedBatchNormalization.py
@@ -5,12 +5,15 @@ from keras import backend as K
 
 class FixedBatchNormalization(Layer):
 
-    def __init__(self, epsilon: float = 1e-3, axis: int = -1,
-                 weights=None, beta_initializer='zeros', gamma_initializer='ones',
+    def __init__(self, epsilon=1e-3, axis=-1,
+                 weights=None, beta_init='zeros', gamma_init='ones',
                  gamma_regularizer=None, beta_regularizer=None, **kwargs):
+        """
+        Initialisation du Batch Normalization fixe.
+        """
         self.supports_masking = True
-        self.beta_initializer = initializers.get(beta_initializer)
-        self.gamma_initializer = initializers.get(gamma_initializer)
+        self.beta_init = initializers.get(beta_init)
+        self.gamma_init = initializers.get(gamma_init)
         self.epsilon = epsilon
         self.axis = axis
         self.gamma_regularizer = regularizers.get(gamma_regularizer)
@@ -19,20 +22,23 @@ class FixedBatchNormalization(Layer):
         super(FixedBatchNormalization, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        """
+        Crée les poids nécessaires pour la normalisation par lot.
+        """
         self.input_spec = [InputSpec(shape=input_shape)]
         shape = (input_shape[self.axis],)
 
         self.gamma = self.add_weight(
             name='{}_gamma'.format(self.name),
             shape=shape,
-            initializer=self.gamma_initializer,
+            initializer=self.gamma_init,
             regularizer=self.gamma_regularizer,
             trainable=False
         )
         self.beta = self.add_weight(
             name='{}_beta'.format(self.name),
             shape=shape,
-            initializer=self.beta_initializer,
+            initializer=self.beta_init,
             regularizer=self.beta_regularizer,
             trainable=False
         )
@@ -55,29 +61,32 @@ class FixedBatchNormalization(Layer):
 
         self.built = True
 
-    def call(self, inputs, mask=None):
+    def call(self, x, mask=None):
+        """
+        Applique la normalisation par lot sur l'entrée `x`.
+        """
         assert self.built, 'Layer must be built before being called'
-        input_shape = K.int_shape(inputs)
+        input_shape = K.int_shape(x)
 
         reduction_axes = list(range(len(input_shape)))
         del reduction_axes[self.axis]
         broadcast_shape = [1] * len(input_shape)
         broadcast_shape[self.axis] = input_shape[self.axis]
 
-        if sorted(reduction_axes) == list(range(K.ndim(inputs))[:-1]):
+        if sorted(reduction_axes) == list(range(K.ndim(x))[:-1]):
             x_normed = K.batch_normalization(
-                inputs, self.running_mean, self.running_std,
+                x, self.running_mean, self.running_std,
                 self.beta, self.gamma,
                 epsilon=self.epsilon
             )
         else:
-            # Need broadcasting
+            # Besoin de diffusion (broadcasting)
             broadcast_running_mean = K.reshape(self.running_mean, broadcast_shape)
             broadcast_running_std = K.reshape(self.running_std, broadcast_shape)
             broadcast_beta = K.reshape(self.beta, broadcast_shape)
             broadcast_gamma = K.reshape(self.gamma, broadcast_shape)
             x_normed = K.batch_normalization(
-                inputs, broadcast_running_mean, broadcast_running_std,
+                x, broadcast_running_mean, broadcast_running_std,
                 broadcast_beta, broadcast_gamma,
                 epsilon=self.epsilon, axis=0
             )
@@ -85,6 +94,9 @@ class FixedBatchNormalization(Layer):
         return x_normed
 
     def get_config(self):
+        """
+        Renvoie la configuration du calque pour une éventuelle sérialisation.
+        """
         config = {
             'epsilon': self.epsilon,
             'axis': self.axis,
